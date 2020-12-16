bbefc1cbfedf9c6d694d6405579b75a42767865c
Jjin SoL <parkleydondon@yahoo.com>
2020-12-16 00:19:53 -0800

    Added follow/unfollow feature among users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,30 @@
 const User = require('../models/User');
 const Post = require('../models/Post');
+const Follow = require('../models/Follow');
+
+exports.sharedProfileData = async function(req, res, next) {
+  let isVisitorProfile = false;
+  let isFollowing = false;
+  if (req.session.user) { // Check if this visitor is logged in
+    console.log("profile userid: " , req.profileUser._id);
+    console.log("visitorid: ", req.visitorId);
+    isVisitorProfile = req.profileUser._id.equals(req.visitorId);
+    isFollowing = await Follow.isVisitorFollowing(req.profileUser._id, req.visitorId)
+  }
+  req.isVisitorProfile = isVisitorProfile;
+  req.isFollowing = isFollowing;
+
+  // Retrive post, followers, followings counts
+  let postCountPromise = Post.countPostsByAuthor(req.profileUser._id);
+  let followerCountPromise = Follow.countFollowerByAuthor(req.profileUser._id);
+  let followingCountPromise = Follow.countFollowingByAuthor(req.profileUser._id);
+
+  let [postCount, followerCount, followingCount] = await Promise.all([postCountPromise, followerCountPromise, followingCountPromise]);
+  req.postCount = postCount;
+  req.followerCount = followerCount;
+  req.followingCount = followingCount;
+  next();
+}
 
 exports.mustBeLoggedIn = function(req, res, next) {
   if (req.session.user) {
@@ -56,9 +81,11 @@ exports.logout = function(req, res) {
 }
 
 
-exports.displayHome = function(req, res) {
+exports.displayHome = async function(req, res) {
   if (req.session.user) {
-    res.render('home-dashboard');
+    // Fetch homepage feed for current user
+    let posts = await Post.getPosts(req.session.user._id);
+    res.render('home-dashboard', {posts: posts});
   } else {
     // Access err msg & save it to the new object and delete the msg from session, hence flash
     res.render('home-guest', {regErrors: req.flash('regErrors')});
@@ -79,11 +106,53 @@ exports.ifUserExists = function(req, res, next) {
 exports.displayProfileHome = function(req, res) {
   Post.findByAuthorId(req.profileUser._id).then(function(posts) {
     res.render('profile', {
+      currentPage: "posts",
       posts: posts,
       profileUsername: req.profileUser.username,
-      profileAvatar: req.profileUser.avatar
+      profileAvatar: req.profileUser.avatar,
+      isFollowing: req.isFollowing,
+      isVisitorProfile: req.isVisitorProfile,
+      counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
     })
   }).catch(function() {
     res.render('404')
   });
+}
+
+
+exports.displayFollowers = async function(req, res) {
+  try {
+    let followers = await Follow.getFollowersById(req.profileUser._id);
+    res.render('profile-followers', {
+      currentPage: "followers",
+      followers: followers,
+      profileUsername: req.profileUser.username,
+      profileAvatar: req.profileUser.avatar,
+      isFollowing: req.isFollowing,
+      isVisitorProfile: req.isVisitorProfile,
+      counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
+    });
+  } catch(err) {
+    console.log(err);
+    res.render('404');
+  }
+}
+
+
+
+exports.displayFollowings = async function(req, res) {
+  try {
+    let followings = await Follow.getFollowingsById(req.profileUser._id);
+    res.render('profile-followings', {
+      currentPage: "followings",
+      followings: followings,
+      profileUsername: req.profileUser.username,
+      profileAvatar: req.profileUser.avatar,
+      isFollowing: req.isFollowing,
+      isVisitorProfile: req.isVisitorProfile,
+      counts: {postCount: req.postCount, followerCount: req.followerCount, followingCount: req.followingCount}
+    });
+  } catch {
+    res.render('404');
+  }
 }
--- db.js
+++ db.js
@@ -2,13 +2,24 @@
 const mongodb = require('mongodb');
 const dotenv = require('dotenv');
 
+
+
 dotenv.config();
 
 const connectionString = process.env.CONNECTIONSTRING;
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   module.exports = client;
   //console.log(err);
+
   // Only when the db connection is successful
   const app = require('./app');
   app.listen(process.env.PORT);
 });
+
+// 
+// const mongoose = require('mongoose')
+// mongoose.connect("mongodb://localhost:27017/circleDB", { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
+//   module.exports = client;
+//   const app = require('./app')
+//   app.listen(process.env.PORT)
+// });
